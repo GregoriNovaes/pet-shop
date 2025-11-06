dce52c54e8100d79f78df356ba2e15dca22d46e2
Gregori Cabral <gregoricabral@Gregoris-MacBook-Air.local>
2025-11-06 11:04:41 -0300

    Finalizado toda a parte de estilização do site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
-export default function Home() {
+import { AppointmentForm } from '@/components/appointment-form';
+import { PeriodSection } from '@/components/period-section';
+import { prisma } from '@/lib/prisma';
+import { APPOINTMENT_DATA, groupAppointmentByPeriod } from '@/utils';
+
+export default async function Home() {
+  const appoint = await prisma.appointment.findMany();
+
+  const periods = groupAppointmentByPeriod(APPOINTMENT_DATA);
+
   return (
-    <div>
-      <h2>Rocketseat 🚀</h2>
+    <div className="bg-background-primary p-6">
+      <div className="flex items-center justify-between mb-8">
+        <div>
+          <h1 className="text-title-size text-contet-primary mb-2">
+            Sua Agenda
+          </h1>
+          <p className="text-paragraph-medium-size text-content-secondary">
+            Aqui você pode ver todos os clientes e serviços agendados para hoje.
+          </p>
+        </div>
+      </div>
+      <div className="pb-24 md:pb-0">
+        {periods.map((period, index) => (
+          <PeriodSection key={index} period={period} />
+        ))}
+      </div>
+
+      <div className="fixed bottom-0 left-0 right-0 flex justify-center bg-[#23242C] py-[18px] px-6 md:bottom-6 md:right-6 md:left-auto md:top-auto md:w-auto md:bg-transparent md:p-0">
+        <AppointmentForm />
+      </div>
     </div>
   );
 }
